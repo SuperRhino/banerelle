5633885fbf28265397510eba0269ac50d98dccc8
Pasto <rpastorelle@yahoo.com>
2015-10-12 21:44:57 -0400

    Add index

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+include('index.html');
+//function main()
+//{
+//    //    error_reporting(E_ALL);
+//    date_default_timezone_set('UTC');
+//    require '../vendor/autoload.php';
+//    $app = new Core\Application(realpath(__DIR__.'/../'));
+//    $app->run();
+//}
+//main();
