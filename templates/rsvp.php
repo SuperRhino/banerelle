21b9b9b4325a9daf7bb6f15c49af709ca1c0aa9d
Pasto <rpastorelle@yahoo.com>
2015-10-19 22:46:48 -0400

    Build RSVP form out

--- templates/rsvp.php
+++ templates/rsvp.php
@@ -2,13 +2,44 @@
 
 ?>
 <div class="row">
-    <form class="navbar-form navbar-right" role="form">
-        <div class="form-group">
-            <input type="text" placeholder="Email" class="form-control">
+    <h1>RSVP <small>Banerelle Wedding</small></h1>
+    <p class="lead">We'd love for you to join us. Start by finding your name:</p>
+    <form class="" role="form">
+        <!-- add .has-success or .has-error -->
+        <div class="form-group has-success has-feedback">
+            <input id="inputLastName" class="form-control input-lg" type="text" placeholder="Last name">
+            <!-- .glyphicon-ok or .glyphicon-remove -->
+            <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
         </div>
+        <blockquote>
+            <p>Pick Your Eats</p>
+        </blockquote>
         <div class="form-group">
-            <input type="password" placeholder="Password" class="form-control">
+            <div class="radio">
+                <label>
+                    <input type="radio" name="foodOptions" id="foodOptions1" value="Burrito">
+                    Breakfast Burrito
+                </label>
+            </div>
+            <div class="radio">
+                <label>
+                    <input type="radio" name="foodOptions" id="foodOptions2" value="McMuffin">
+                    Egg McMuffin
+                </label>
+            </div>
         </div>
-        <button type="submit" class="btn btn-success">Sign in</button>
+        <blockquote>
+            <p>Guest</p>
+        </blockquote>
+        <div class="form-group">
+            <div class="checkbox disabled">
+                <label>
+                    <input type="checkbox" value="+1" disabled>
+                    +1
+                </label>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-success">YES</button>
+        <button type="button" class="btn btn-danger">Nope</button>
     </form>
 </div>
