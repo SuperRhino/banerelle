a485f3e5332c0fabaaed498742452e0216d6481c
Pasto <rpastorelle@yahoo.com>
2015-12-04 11:08:52 -0500

    Add bunches of TODOs

--- src/App/Controllers/Api/AccountController.php
+++ src/App/Controllers/Api/AccountController.php
@@ -8,5 +8,31 @@ class AccountController extends BaseApiController
     public function login()
     {
         return $this->success($this->json());
+
+        $username = $this->json('username');
+        $password = $this->json('password');
+
+        // TODO --- Login User
+        // 1. select from users table where username and password
+        // 2. if not found, throw error
+        // 3. --- Create [Db] Session ---
+        // 4. set sessionid cookie (hashid of sessions.id)
+        // 5. respond / redirect
+    }
+
+    public function validate()
+    {
+        // TODO --- Validate session
+        // 1. if not found locally (sessionid cookie), throw error / redirect
+        // 2. find userId from sessions
+        // 3. if valid, reset cookie expiration
+    }
+
+    public function logout()
+    {
+        // TODO --- Clean Session
+        // 1. find/delete session from sessionid cookie
+        // 2. delete/expire cookie
+        // 3. respond / redirect
     }
 }
