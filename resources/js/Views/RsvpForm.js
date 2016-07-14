d2eb918cae353f7ed0d11b0b4f4b706bc787f001
Pasto <rpastorelle@yahoo.com>
2016-07-13 22:48:14 -0400

    Add jumbotron buttons on home. Allow for hidden pages. Fix save button in admin.

--- resources/js/Views/RsvpForm.js
+++ resources/js/Views/RsvpForm.js
@@ -7,7 +7,9 @@ export default class RsvpForm extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      open: false,
+    };
 
     this.onYes = this.onYes.bind(this);
     this.onNope = this.onNope.bind(this);
@@ -19,44 +21,64 @@ export default class RsvpForm extends React.Component {
     return (
       <div className="row">
           <h1>RSVP <small>Banerelle Wedding</small></h1>
-          <p className="lead">{"We'd love for you to join us. Start by finding your name:"}</p>
-          <form className="" role="form">
-              <div className="form-group has-success has-feedback">
-                  <input id="inputLastName" className="form-control input-lg" type="text" placeholder="Last name" />
-                  <span className="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
-              </div>
-              <blockquote>
-                  <p>Pick Your Eats</p>
-              </blockquote>
-              <div className="form-group">
-                  <div className="radio">
-                      <label>
-                          <input type="radio" name="foodOptions" id="foodOptions1" value="Burrito" />
-                          Breakfast Burrito
-                      </label>
-                  </div>
-                  <div className="radio">
-                      <label>
-                          <input type="radio" name="foodOptions" id="foodOptions2" value="McMuffin" />
-                          Egg McMuffin
-                      </label>
-                  </div>
-              </div>
-              <blockquote>
-                  <p>Guest</p>
-              </blockquote>
-              <div className="form-group">
-                  <div className="checkbox disabled">
-                      <label>
-                          <input type="checkbox" value="+1" disabled />
-                          +1
-                      </label>
-                  </div>
-              </div>
-              <button type="submit" className="btn btn-success" onClick={this.onYes}>YES</button>
-              <button type="button" className="btn btn-danger" onClick={this.onNope}>Nope</button>
-          </form>
+          {this.renderMessage()}
+          {this.renderForm()}
       </div>
+    );
+  }
+
+  renderMessage() {
+    return null; // TODO: Remove
+    return (
+      <p className="lead">{"We'd love for you to join us. Start by finding your name:"}</p>
+    );
+  }
+
+  renderForm() {
+    if (! this.state.open) {
+      return (
+        <div className="alert alert-info">
+          {"You'll be able to RSVP as soon as we invite you ;)"}
+        </div>
+      );
+    }
+    return (
+      <form className="" role="form">
+          <div className="form-group has-success has-feedback">
+              <input id="inputLastName" className="form-control input-lg" type="text" placeholder="Last name" />
+              <span className="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+          </div>
+          <blockquote>
+              <p>Pick Your Eats</p>
+          </blockquote>
+          <div className="form-group">
+              <div className="radio">
+                  <label>
+                      <input type="radio" name="foodOptions" id="foodOptions1" value="Burrito" />
+                      Breakfast Burrito
+                  </label>
+              </div>
+              <div className="radio">
+                  <label>
+                      <input type="radio" name="foodOptions" id="foodOptions2" value="McMuffin" />
+                      Egg McMuffin
+                  </label>
+              </div>
+          </div>
+          <blockquote>
+              <p>Guest</p>
+          </blockquote>
+          <div className="form-group">
+              <div className="checkbox disabled">
+                  <label>
+                      <input type="checkbox" value="+1" disabled />
+                      +1
+                  </label>
+              </div>
+          </div>
+          <button type="submit" className="btn btn-success" onClick={this.onYes}>YES</button>
+          <button type="button" className="btn btn-danger" onClick={this.onNope}>Nope</button>
+      </form>
     );
   }
 
